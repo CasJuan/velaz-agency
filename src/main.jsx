3d1ee1d08635c9e02e4d1ce439c2fe355fbcf405
JuanC <castrojuanig25@gmail.com>
2026-04-28 19:36:53 -0300

    hero mas nav resposive

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Nav } from './Nav'
 import { Hero } from './Hero'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Nav/>
     <Hero/>
   </StrictMode>,
 )
